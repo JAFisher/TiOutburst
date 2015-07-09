3f9f562a34cb20918b8557254687bde71e5e4be7
jamie fisher <devjamiefisher@gmail.com>
2015-07-09 16:38:15 +0100

    setting up example

---
+++ app/controllers/index.js
@@ -0,0 +1,49 @@
+var utter = require("com.jamiefisher.utterance");
+
+
+
+utter.setup({
+    volume: 10,
+    rate: 1,
+    voice: null
+});
+
+
+utter.talk({
+    rate: 0.2,
+    text: "At the start of time there was...",
+    volume: 0.5
+},{
+    rate: 0.1,
+    text: "The BIG BANG",
+    volume: 1
+},{
+    rate: 0.2,
+    text: "Then jeff sniffed an walnut.",
+    volume: 1
+},{
+   rate: 0.2,
+   text: "And the world was created...",
+   volume: 1
+}, {
+   rate: 0.2,
+   text: "Vending machines rose up against the great Jeff empire.",
+   volume: 1
+},{
+    rate: 0.2,
+    text: "however their delicious food only made him stronger.",
+    volume: 1
+},{
+    rate: 0.2,
+    text: "he ruled with an iron fist for the next thousand years...",
+    volume: 1
+}
+);
+
+
+
+function doClick(e) {
+    alert($.label.text);
+}
+
+$.index.open();
